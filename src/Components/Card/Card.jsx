1c313710ae41c7845ae2360b100579060f703f78
Vasjan Çupri <vasjancupri04@gmail.com>
2023-10-10 21:53:47 +0200

    Added proptype validation

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,14 +1,22 @@
 import styles from "./Card.module.css"
+import PropTypes from "prop-types"
 
-export default function Card({title, imageSrc, category, price}) {
+export default function Card({ title, imageSrc, category, price }) {
   return (
     <div className={styles.card}>
-        <img src={imageSrc} alt={title} />
-        <div>
-            <h2>{title}</h2>
-            <p>{category}</p>
-            <h4>{price} €</h4>
-        </div>
+      <img src={imageSrc} alt={title} />
+      <div>
+        <h2>{title}</h2>
+        <p>{category}</p>
+        <h4>{price} €</h4>
+      </div>
     </div>
   )
+}
+
+Card.propTypes = {
+  title: PropTypes.string,
+  imageSrc: PropTypes.string,
+  category: PropTypes.string,
+  price: PropTypes.string,
 }
